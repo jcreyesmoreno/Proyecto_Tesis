9ecc44648c825c316eb4a3eb2585969a82433174
jcreyesmoreno <wolverine1401@gmail.com>
2016-03-13 18:00:31 -0600

    Update searchtesisuser.blade.php
    Filtro para las columnas a traves de los campos de busqueda

--- app/views/searchtesisuser.blade.php
+++ app/views/searchtesisuser.blade.php
@@ -12,39 +12,74 @@
     @endif
 	@if(isset($Thesis))
 		<div class="table">
-			<h2 align="center">RESULTADOS DE LA BÚSQUEDA</h2>
+			<h2 align="center">RESULTADOS DE LA BÚSQUEDA por
+        @if(isset($fields['title']))
+          TITULO: {{ $fields['title'] }}
+        @endif
+
+        @if(isset($fields['career']))
+          CARRERA: {{ $fields['career'] }}
+        @endif
+
+        @if(isset($fields['author']))
+          AUTOR: {{ $fields['author'] }}
+        @endif
+
+        @if(isset($fields['year']))
+          AÑO: {{ $fields['year'] }}
+        @endif
+      </h2>
 		</div>
 		<table class="table table-hover table-bordered" cellspacing="2">
-			<tr>
-				<th class="num">N°</th>
-				<th class="tit">TITULO</th>
-				<th class="carr">CARRERA</th>
-				<th class="carr">AUTOR</th>
-				<th class="carr">AÑO</th>
-				<th class="ind">INDICE</th>
-				<th class="prol">PROLOGO</th>
-				<th class="tes">TESIS</th>
-			</tr>
-		@for ($i = 0; $i < count($Thesis); $i++)
-			<tr>
-				<td class="view">{{ $i + 1 }}</td>
-				<td class="view">{{ $Thesis[$i]->title }}</td>
-				<td class="view">{{ $Thesis[$i]->career }}</td>
-				<td class="view">{{ $Thesis[$i]->author }}</td>
-				<td class="view">{{ $Thesis[$i]->year }}</td>
-				<td class="ver"><a href="/indice/pdf/{{ $Thesis[$i]->id }}">VER</a></td>
-				<td class="ver"><a href="/prologo/pdf/{{ $Thesis[$i]->id }}">VER</a></td>
-				<td class="ver"><a href="/tesis/pdf/{{ $Thesis[$i]->id }}">VER</a></td>
-				<td class="del">
-
-					</form>
-				</td>
-			</tr>
-		@endfor
+      <thead>
+  			<tr>
+  				<th class="num">N°</th>
+          @if(!isset($fields['title']))
+  				  <th class="tit">TITULO</th>
+          @endif
+          @if(!isset($fields['career']))
+  				  <th class="carr">CARRERA</th>
+          @endif
+          @if(!isset($fields['author']))
+            <th class="carr">AUTOR</th>
+          @endif
+          @if(!isset($fields['year']))
+            <th class="carr">AÑO</th>
+          @endif
+  				<th class="ind">INDICE</th>
+  				<th class="prol">PROLOGO</th>
+  				<th class="tes">TESIS</th>
+  			</tr>
+      </thead>
+      <tbody>
+  		@for ($i = 0; $i < count($Thesis); $i++)
+  			<tr>
+  				<td class="view">{{ $i + 1 }}</td>
+          @if(!isset($fields['title']))
+  				  <td class="view">{{ $Thesis[$i]->title }}</td>
+          @endif
+          @if(!isset($fields['career']))
+  				  <td class="view">{{ $Thesis[$i]->career }}</td>
+          @endif
+          @if(!isset($fields['author']))
+  				  <td class="view">{{ $Thesis[$i]->author }}</td>
+          @endif
+          @if(!isset($fields['year']))
+  				  <td class="view">{{ $Thesis[$i]->year }}</td>
+          @endif
+  				<td class="ver"><a href="/indice/pdf/{{ $Thesis[$i]->id }}">VER</a></td>
+  				<td class="ver"><a href="/prologo/pdf/{{ $Thesis[$i]->id }}">VER</a></td>
+  				<td class="ver"><a href="/tesis/pdf/{{ $Thesis[$i]->id }}">VER</a></td>
+  				<td class="del">
+  					</form>
+  				</td>
+  			</tr>
+  		@endfor
+      <tbody>
 		</table>
 		<br />
 		<form action="/searchtesisuser" method="get">
-			<!--<p align="center">     
+			<!--<p align="center">
     		<input type="submit" class="btn btn-success btn-lg" value="Regresar" class="regresar">
   			</p>-->
   			<p align="center">
@@ -53,6 +88,6 @@
           </button>	</p>
   		</form>
 	@else
-		<p>No se ha realizado ninguna consulta</p>	
+		<p>No se ha realizado ninguna consulta</p>
 	@endif
 </section>
